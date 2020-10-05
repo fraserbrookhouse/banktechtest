c0748052bfeed51ea22c5706e5676389a82e8242
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-10-05 12:13:45 +0100

    wrote failing test for withdraw method

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -8,4 +8,12 @@ describe "#Bank" do
       expect(bank.balance).to eq(50)
     end
   end
+  describe "#withdraw" do
+    it "takes a number and subtracts it from the balance" do
+      bank = Bank.new
+      bank.deposit(100)
+      bank.withdraw(20)
+      expect(bank.balance).to eq(80)
+    end
+  end
 end
