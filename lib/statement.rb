1249cb371aa8f3a1e654d478de59da5084218f4a
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-10-06 19:40:28 +0100

    added test for new class statement to extract statement printing from bank class, working

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,6 +1,18 @@
+
+
 class Statement
-  
-  def self.print(transaction_history)
-    
+
+  def self.print_statement(transactions)
+    statement = "date || credit || debit || balance\n"
+    transactions.each do |transaction|
+      if transaction.key?(:credit)
+        string = "#{transaction[:date]} || #{format('%.2f', transaction[:credit])} || || #{format('%.2f', transaction[:balance])}"
+      else
+        string = "#{transaction[:date]} || || #{format('%.2f', transaction[:debit])} || #{format('%.2f', transaction[:balance])}"
+      end
+      statement += string
+      statement += "\n"
+    end
+    print statement
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,16 +1,20 @@
+# frozen_string_literal: true
+
 require 'statement'
 
 describe '#Statement' do
-  let(:first_transaction) { double :transaction, date: "25/06/2020", credit: 500, balance: 500}
-  let(:second_transaction) { double :transaction, date: "13/08/2020", debit: 100, balance: 400}
-  let(:transaction_history) { [first_transaction, second_transaction] }
-  describe '#print' do
+  describe '#print_statement' do
+    transaction_history = [
+      { date: '25/06/2020', credit: 500, balance: 500 },
+      { date: '13/08/2020', debit: 100, balance: 400 }
+    ]
     it 'prints a correctly formatted statement for credit and devit transactions' do
       statement = <<~STATEMENT
         date || credit || debit || balance
-        23/06/2020 || 50.00 || || 20.00
+        25/06/2020 || 500.00 || || 500.00
+        13/08/2020 || || 100.00 || 400.00
       STATEMENT
-      expect { Statement.print(transaction_history) }.to output(statement).to_stdout
+      expect { Statement.print_statement(transaction_history) }.to output(statement).to_stdout
     end
   end
 end
